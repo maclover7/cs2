748cffcc2fec1cea8c400ecc9443d245ade53d28
Jon Moss <maclover7@users.noreply.github.com>
2015-10-08 09:37:46 -0400

    Update assignment-12.java

--- assignment-12.java
+++ assignment-12.java
@@ -20,6 +20,7 @@ public class DateFunctions {
      */
     public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
+      // Normal --> Julian
       System.out.println("Please enter the month (01 for January, 10 for October):");
       int month = sc.nextInt() - 1;
       System.out.println("Please enter the date (01 for the 1st, 10 for the 10th):");
@@ -27,16 +28,34 @@ public class DateFunctions {
       System.out.println("Please enter the year:");
       int year = sc.nextInt();
       int julian_date = dateToJulian(month, date, year);
-      System.out.println("The julian date is " + julian_date + ".");
-      
+      System.out.println("The julian date is " + julian_date + ".\n");
+      // Julian --> Normal
       String regular_date = julianToDate(1);
-      System.out.println("The regular date is " + regular_date + ".");
+      System.out.println("The regular date is " + regular_date + ".\n");
+      // # of days between 2 dates
+      // input first date
+      System.out.println("Enter the information for the first date:");
+      System.out.println("Please enter the month (01 for January, 10 for October):");
+      int month1 = sc.nextInt() - 1;
+      System.out.println("Please enter the date (01 for the 1st, 10 for the 10th):");
+      int date1 = sc.nextInt();
+      System.out.println("Please enter the year:");
+      int year1 = sc.nextInt();
+      // input second date
+      System.out.println("Enter the information for the second date:");
+      System.out.println("Please enter the month (01 for January, 10 for October):");
+      int month2 = sc.nextInt() - 1;
+      System.out.println("Please enter the date (01 for the 1st, 10 for the 10th):");
+      int date2 = sc.nextInt();
+      System.out.println("Please enter the year:");
+      int year2 = sc.nextInt();
+      int days_between = daysBetween(month1, date1, year1, month2, date2, year2);
+      System.out.println("The days between the two dates is " + days_between + ".\n");
     }
 
     public static int dateToJulian(int month, int date, int year){
       Calendar c1 = GregorianCalendar.getInstance();
-      c1.set(year, month, date);  //January 30th 2000
-      Date sDate = c1.getTime();
+      c1.set(year, month, date);
       int day_of_year = c1.get(Calendar.DAY_OF_YEAR);
       return year + day_of_year;
     }
@@ -50,7 +69,24 @@ public class DateFunctions {
       String str_date = Integer.toString(date);
       String str_month = Integer.toString(month);
       String str_year = Integer.toString(year);
-      
       return str_date + str_month + str_year;
+    }
+    
+    public static int daysBetween(int month1, int date1, int year1, int month2, int date2, int year2){
+      Calendar c1 = GregorianCalendar.getInstance();
+      c1.set(year1, month1, date1);
+      int day_of_year1 = c1.get(Calendar.DAY_OF_YEAR);
+      Calendar c2 = GregorianCalendar.getInstance();
+      c2.set(year2, month2, date2);
+      int day_of_year2 = c2.get(Calendar.DAY_OF_YEAR);
+      if (day_of_year1 < day_of_year2){
+        int difference = day_of_year1 - day_of_year2;
+        int result = (difference < 0 ? -difference : difference);
+        return result;
+      } else {
+        int difference = day_of_year1 - day_of_year2;
+        int result = (difference < 0 ? -difference : difference);
+        return result;
+      }
     }
 }
