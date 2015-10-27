83e8ad7d45a25011bcf799490120b0200f37a5ae
Jon Moss <maclover7@users.noreply.github.com>
2015-10-27 09:27:26 -0400

    Update assignment-18.java

--- assignment-18.java
+++ assignment-18.java
@@ -9,7 +9,13 @@ public class PersonClass {
      System.out.println("Input the height for the first person (as an integer):");
      int ht1 = sc.nextInt();
      System.out.println("Input the weight for the first person (as an integer):");
-     int wt1 = sc.nextInt();
+     int wt1 = sc.nextInt();.
+     System.out.println("Input the age for the first person (as an integer):");
+     int age1 = sc.nextInt();
+     System.out.println("Input the hair color for the first person:");
+     String hair1 = sc.next();
+     System.out.println("Input the name for the first person:");
+     String name1 = sc.next();
      //
      System.out.println("Input the gender for the second person (male or female):");
      String gdr2 = sc.next();
@@ -17,9 +23,16 @@ public class PersonClass {
      int ht2 = sc.nextInt();
      System.out.println("Input the weight for the second person (as an integer):");
      int wt2 = sc.nextInt();
-     //
-     Person p1 = new Person(gdr1, ht1, wt1);
-     Person p2 = new Person(gdr2, ht2, wt2);
+     System.out.println("Input the age for the second person (as an integer):");
+     int age2 = sc.nextInt();
+     System.out.println("Input the hair color for the second person:");
+     String hair2 = sc.next();
+     System.out.println("Input the name for the second person:");
+     String name2 = sc.next();
+     
+     Person p1 = new Person(gdr1, ht1, wt1, age1, hair1, name1, eye1);
+     Person p2 = new Person(gdr2, ht2, wt2, age2, hair2, name2, eye2);
+     
      if (p1.height > p2.height) {
        System.out.println("Person 1 is taller than person 2.");
      } else {
