bcb4c681e1e7b997db7ddb0875890b128287a86a
Jon Moss <maclover7@users.noreply.github.com>
2015-11-20 09:42:10 -0500

    Create assignment-25-part4.java

---
+++ assignment-25-part4.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arithmetic4;
+import java.util.Math;
+import java.util.Scanner;
+/**
+ *
+ * @author 18mossj
+ */
+public class Arithmetic4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Please enter the radius for the circle:");
+      int radius = sc.nextInt();
+      //calculate circumference
+      int circumference = 2 * radius * Math.PI;
+      System.out.println("For a radius of " + radius + ", the circumference is " + circumference + ".");
+      // calculate diameter
+      int diameter = radius * 2;
+      System.out.println("For a radius of " + radius + ", the diameter is " + diameter + ".");
+      // calculate area
+      int area = Math.PI * (radius * radius);
+      System.out.println("For a radius of " + radius + ", the area is " + area + ".");
+    }
+}
