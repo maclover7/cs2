52df76f885265c71993d399af110b21f1c875a7d
Jon Moss <maclover7@users.noreply.github.com>
2016-01-12 10:10:15 -0500

    Update assignment-31.java

--- assignment-31.java
+++ assignment-31.java
@@ -44,11 +44,51 @@ public class Storeproject {
       if (command == "add-item") {
         addItem(sc, store);
       } else if (command == "search-item") {
-        searchItem(sc, store);
+        Item[] items = searchItem(sc, store);
+        
+        if (items.length == 0) {
+          System.out.println("There are no return values.");
+        } else {
+          for (int i = 0; i < items.length; i++){
+            System.out.println("For return value #" + i + ":");
+            System.out.println("The description is " + items[i].getDescription());
+            System.out.println("The buyer price is " + items[i].getBuyerPrice());
+            System.out.println("The sales price is " + items[i].getSalesPrice());
+          }
+        }
+        
       } else if (command == "buy-item") {
-        searchItem(sc, store);
+        Item[] items = searchItem(sc, store);
+        
+        if (items.length == 0) {
+          System.out.println("There are no items available for purchase.");
+        } else {
+          System.out.println("How many would you like to buy?");
+          int purchase_count = sc.nextInt();
+          
+          if (purchase_count > items.length) {
+            System.out.println("You cannot buy more than are in stock.");
+          } else {
+            // remove from Item[] -- don't exist in inventory anymore
+            System.out.println("You have bought " + purchase_count + "items.");
+          }
+        }
       } else if (command == "rm-item") {
-        searchItem(sc, store);
+        Item[] items = searchItem(sc, store);
+        
+        if (items.length == 0) {
+          System.out.println("There are no items available for removal.");
+        } else {
+          System.out.println("How many would you like to remove?");
+          int remove_count = sc.nextInt();
+          
+          if (remove_count > items.length) {
+            System.out.println("You cannot remove more than are in stock.");
+          } else {
+            // remove from Item[] -- don't exist in inventory anymore
+            System.out.println("You have removed " + remove_count + "items.");
+          }
+        }
       } else if (command == "exit") {
         System.exit(0);
       }
@@ -67,7 +107,7 @@ public class Storeproject {
       System.out.println("Item added!");
     }
     
-    public static void searchItem(Scanner sc, Store store) {
+    public static Item[] searchItem(Scanner sc, Store store) {
       System.out.println("What would you like to search for the item by?");
       System.out.println("Enter desc for description");
       System.out.println("Enter bp for buyer_price");
@@ -100,6 +140,8 @@ public class Storeproject {
           }
         }
       }
+      
+      return return_values;
     }
 }
 
