86fea2e53d316145738d1301bab88892cdd34be5
Jon Moss <maclover7@users.noreply.github.com>
2016-02-11 10:03:15 -0500

    Update assignment-33-part7.java

--- assignment-33-part7.java
+++ assignment-33-part7.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package scorer;
+import java.util.HashMap;
 import java.util.Scanner;
 /**
  *
@@ -15,13 +16,34 @@ public class Scorer {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+      int global_average = 0;
       Scanner sc = new Scanner(System.in);
       System.out.println("How many students would you like to calculate for?");
       int student_count = sc.nextInt();
       
       for(int i = 0; i < student_count; i++) {
+        // Retrieve data
+        int student_counter = 0;
         System.out.println("Please enter the following data for Student #" + i + ":");
+        
         System.out.println("Please enter the first test score:");
+        student_counter += sc.nextInt();
+        System.out.println("Please enter the second test score:");
+        student_counter += sc.nextInt();
+        System.out.println("Please enter the third test score:");
+        student_counter += sc.nextInt();
+        System.out.println("Please enter the fourth test score:");
+        student_counter += sc.nextInt();
+        
+        // Output student's personal average
+        int student_average = (student_counter / 4);
+        System.out.println("Student #" + i + "'s average is " + student_average + ".");
+        
+        // Log to global average
+        global_average += student_counter;
       }
+      
+      // Output all students' (class) average
+      System.out.println("The class average is " + (global_average / student_count) + ".");
     }
 }
