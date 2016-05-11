c255e62a4dc0274131077b3b92fa0d81f8523509
Jon Moss <me@jonathanmoss.me>
2016-05-11 14:10:17 -0400

    A34P3 edits to match assignment

--- assignment-34-part-3.rb
+++ assignment-34-part-3.rb
@@ -1,8 +1,12 @@
 require 'pathname'
+require 'ostruct'
 
 class Application
   ANSWERS_FILE = "#{Pathname.pwd}/testanswers.txt"
   RESPONSES_FILE = "#{Pathname.pwd}/testresponses.txt"
+
+  class Student < Struct.new(:correct, :id, :responses)
+  end
 
   def run
     puts 'Processing test answer data...'
@@ -12,16 +16,29 @@ class Application
     responses = File.readlines(RESPONSES_FILE)
     responses.each { |n| n.delete!("\n") }
 
-    total_responses = responses.length
-    correct = 0
+    # Create a student object for every line of responses
+    students = []
+    responses.each do |res|
+      res = res.split(' ')
 
-    total_responses.times do |i|
-      if answers[i] == responses[i]
-        correct += 1
-      end
+      stud = Student.new
+      stud.id = res.delete(res[0])
+      stud.responses = res
+      stud.correct = 0
+      students << stud
     end
 
-    puts "RESULTS: You have #{correct} correct answers"
+    students.each do |student|
+      responses = student.responses
+      responses.length.times do |i|
+        if answers[i] == responses[i]
+          student.correct += 1
+        end
+      end
+
+      puts "RESULTS:"
+      puts "#{student.id} got #{student.correct} correct answers"
+    end
   end
 end
 
